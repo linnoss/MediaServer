79a6b136e2d41004cb505662f219da1813b04119
Steven Spencer <steven.spencer@linn.co.uk>
2012-02-01 11:00:45 +0000

    Added empty sys tray app for mac

---
+++ MacOsX/AppDelegate.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+
+namespace MediaServer
+{
+	public partial class AppDelegate : NSApplicationDelegate
+	{
+		public AppDelegate ()
+		{
+		}
+
+		public override void FinishedLaunching (NSObject notification)
+		{
+			// load image for the status item
+			string imageFile = NSBundle.MainBundle.PathForImageResource("SysTrayIcon.png");
+			NSImage image = new NSImage(imageFile);
+			
+			// creating the status item with a length of -2 is equivalent to the call
+			// [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength]
+			iStatusItem = NSStatusBar.SystemStatusBar.CreateStatusItem(-2);
+			iStatusItem.HighlightMode = true;
+			iStatusItem.Menu = StatusMenu;
+			iStatusItem.Image = image;
+		}
+		
+		partial void OpenConfiguration(NSObject aSender)
+		{
+			Console.WriteLine("Open configuration window");
+		}
+
+		private NSStatusItem iStatusItem;
+	}
+}
+
---
+++ MacOsX/AppDelegate.designer.cs
@@ -0,0 +1,28 @@
+// WARNING
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoMac.Foundation;
+
+namespace MediaServer
+{
+	[Register ("AppDelegate")]
+	partial class AppDelegate
+	{
+		[Outlet]
+		MonoMac.AppKit.NSMenu StatusMenu { get; set; }
+
+		[Action ("OpenConfiguration:")]
+		partial void OpenConfiguration (MonoMac.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (StatusMenu != null) {
+				StatusMenu.Dispose ();
+				StatusMenu = null;
+			}
+		}
+	}
+}
